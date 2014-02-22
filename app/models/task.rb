196c6e97c13891ad8aee2f4591681cecedd7e2bc
lukemesiti <lukemesiti@gmail.com>
2014-02-23 10:46:11 +1100

    updated scopes to use new CATEGORIES array

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,13 +2,13 @@ class Task < ActiveRecord::Base
   belongs_to :user
   has_many :notes, dependent: :destroy
 
-  scope :previous_daily, -> { where(category: 'daily').where("created_at <= ?", Time.zone.now.beginning_of_day) }
-  scope :previous_weekly, -> { where(category: 'weekly').where("created_at <= ?", Time.zone.now.beginning_of_week) }
-  scope :previous_yearly, -> { where(category: 'yearly').where("created_at <= ?", Time.zone.now.beginning_of_year) }
+  scope :previous_daily, -> { where(category: CATEGORIES[0]).where("created_at <= ?", Time.zone.now.beginning_of_day) }
+  scope :previous_weekly, -> { where(category: CATEGORIES[1]).where("created_at <= ?", Time.zone.now.beginning_of_week) }
+  scope :previous_yearly, -> { where(category: CATEGORIES[2]).where("created_at <= ?", Time.zone.now.beginning_of_year) }
 
-  scope :current_daily, -> { where(category: 'daily').where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day) }
-  scope :current_weekly, -> { where(category: 'weekly').where(created_at: Time.zone.now.beginning_of_week..Time.zone.now.end_of_week) }
-  scope :current_yearly, -> { where(category: 'yearly').where(created_at: Time.zone.now.beginning_of_year..Time.zone.now.end_of_year) }
+  scope :current_daily, -> { where(category: CATEGORIES[0]).where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day) }
+  scope :current_weekly, -> { where(category: CATEGORIES[1]).where(created_at: Time.zone.now.beginning_of_week..Time.zone.now.end_of_week) }
+  scope :current_yearly, -> { where(category: CATEGORIES[2]).where(created_at: Time.zone.now.beginning_of_year..Time.zone.now.end_of_year) }
 
   CATEGORIES = ['daily', 'weekly', 'yearly']
 end
