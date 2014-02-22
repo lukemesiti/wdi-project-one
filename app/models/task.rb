5f764f1262a55b648c0b78896f80ffdac2c90b76
lukemesiti <lukemesiti@gmail.com>
2014-02-23 10:13:01 +1100

    created categories array in task controller. updated forms accordinly

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,8 +5,10 @@ class Task < ActiveRecord::Base
   scope :previous_daily, -> { where(category: 'daily').where("created_at <= ?", Time.zone.now.beginning_of_day) }
   scope :previous_weekly, -> { where(category: 'weekly').where("created_at <= ?", Time.zone.now.beginning_of_week) }
   scope :previous_yearly, -> { where(category: 'yearly').where("created_at <= ?", Time.zone.now.beginning_of_year) }
-  
+
   scope :current_daily, -> { where(category: 'daily').where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day) }
   scope :current_weekly, -> { where(category: 'weekly').where(created_at: Time.zone.now.beginning_of_week..Time.zone.now.end_of_week) }
   scope :current_yearly, -> { where(category: 'yearly').where(created_at: Time.zone.now.beginning_of_year..Time.zone.now.end_of_year) }
+
+  CATEGORIES = ['daily', 'weekly', 'yearly']
 end
